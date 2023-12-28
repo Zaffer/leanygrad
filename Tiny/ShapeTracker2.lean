0cdef252a89cbc02bc4b2bd92c96b8da3c7b7565
Zaffer <jameswiles@gmail.com>
2023-12-27 20:32:48 -0500

    expand proof

--- Tiny/ShapeTracker2.lean
+++ Tiny/ShapeTracker2.lean
@@ -1,32 +1,52 @@
--- Step 1: Define Shapes and Strides
-structure ShapeTracker :=
-(old_shape : List ℕ)
-(old_stride : List ℕ)
-(merge_old_shape : List ℕ)
--- Here, we assume that Lists can represent tuples of arbitrary length.
+-- Step 1: Expanding upon the ShapeTracker definition
+structure ShapeTracker where
+  old_shape : List ℕ
+  old_stride : List ℕ
+  merge_old_shape : List ℕ
+  valid : old_shape.length = old_stride.length -- This line ensures the lists have the same length
 
--- Step 2: Declare Mergeability Criteria
+-- Step 2: Expanding upon the mergeability predicate
 def areMergeable (shape1 shape2 : ShapeTracker) : Prop :=
-  -- This predicate will be properly defined later
-  true
+  -- This function should check the actual rules for mergeability, e.g., adjacent dimensions.
+  -- It will require a sophisticated function definition that checks for Case 1 and Case 2.
+  let shapes_can_merge := some_complicated_list_condition shape1.old_shape shape2.merge_old_shape;
+  let strides_can_merge := some_complicated_stride_condition shape1.old_stride shape2.old_stride;
+  -- Both shape and stride conditions must be satisfied for mergeability.
+  shapes_can_merge ∧ strides_can_merge
 
--- Step 3: Create Helper Functions/Lemmas
-lemma non_zero_strides_mergeable {st1 st2 s1 : ℕ} (h_non_zero_strides : st1 = st2 * s1) :
-  -- Complete this lemma based on the conditions provided
-  true
+-- Step 3: Creating helper lemmas for non-zero and zero strides mergeability
+-- These should closely follow the logic given in the reshape_without_symbolic.md
+
+-- Drawing from the criteria specified, here are simplified examples:
+lemma non_zero_strides_mergeable {st1 st2 s1 : ℕ} (h_non_zero_strides : st1 = st2 * s1 ∧ st1 ≠ 0) :
+  -- Conclusion about their mergeability according to the specified condition
+  true -- Placeholder for now
 
 lemma zero_strides_mergeable {st1 st2 : ℕ} (h_zero_strides : st1 = 0 ∨ st2 = 0) :
-  -- Complete this lemma based on the conditions for zero strides
-  true
+  -- Conclusion about their mergeability according to the specified condition
+  true -- Placeholder for now
 
--- Step 4: Formulate Main Theorem
-theorem shapeTracker_mergeable_theorem (shape1 shape2 : ShapeTracker) :
-  areMergeable shape1 shape2 :=
-  -- Proof of the theorem using the lemmas
-  sorry -- sorry is used in Lean to denote incomplete proofs
+-- Step 4: Prove that two given ShapeTrackers are mergeable
+theorem shapeTracker_mergeable_theorem {shape1 shape2 : ShapeTracker} (h : areMergeable shape1 shape2) :
+  -- Mergeability conclusion based on areMergeable predicate
+  true := by
+  -- Here we would spell out the proof using the lemmas and functions defined above
+  sorry -- As before, 'sorry' will be replaced by an actual proof
 
--- Step 5: Build Proof
--- We'd write the actual proofs of our predicates and lemmas here.
+-- As noted earlier, this is a high-level view of the components needed for your proof.
+-- You’ll need to fill in the details for the predicates `areMergeable`, `non_zero_strides_mergeable`, and `zero_strides_mergeable`.
+-- These will include the specific logic based on the mergeability conditions described in the document.
 
--- Step 6: Test by Counterexample
--- Optionally, we'd write code to generate counterexamples if there are any.
+don't know how to synthesize implicit argument
+  @List.length ?m.33 old_shape
+context:
+old_shape : {ℕ : Type u_1} → List ℕ
+old_stride : {ℕ : Type u_2} → List ℕ
+merge_old_shape : {ℕ : Type u_3} → List ℕ
+⊢ Type u_1 Lean 4
+don't know how to synthesize placeholder
+context:
+old_shape : {ℕ : Type u_1} → List ℕ
+old_stride : {ℕ : Type u_2} → List ℕ
+merge_old_shape : {ℕ : Type u_3} → List ℕ
+⊢ Type u_1 Lean 4
